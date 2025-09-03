ba493d8a3787fa836f52527c63c462fa2af5c3df
jinlovespho <msjchr922@gmail.com>
2025-09-03 20:48:34 +0900

    250903 moving from server8 to server12

--- run_script/val_script/run_val_terediff_textzoom.sh
+++ run_script/val_script/run_val_terediff_textzoom.sh
@@ -1,3 +1,3 @@
 
-CUDA_VISIBLE_DEVICES=1 accelerate launch val_textzoom.py         --config configs/val/val_terediff_textzoom.yaml \
+CUDA_VISIBLE_DEVICES=0 accelerate launch val_textzoom.py         --config configs/val/val_terediff_textzoom.yaml \
                                                                 --config_testr testr/configs/TESTR/TESTR_R_50_Polygon.yaml \
